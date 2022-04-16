d882975177b64db4dd223a0a7bf4ed8dfedb9cc9
ruan-pb <ruancruzsoares@hotmail.com>
2022-04-16 15:12:57 -0300

    Componetes Delete e update

--- angular/crud/frontend/src/app/app-routing.module.ts
+++ angular/crud/frontend/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -15,6 +17,16 @@ const routes: Routes = [{
 },{
   path:"products/create",
   component:ProductCreateComponent
+},
+{
+  path:"products/update/:id",
+  component:ProductUpdateComponent
+
+},
+{
+  path:"products/delete/:id",
+  component:ProductDeleteComponent
+
 }];
 
 @NgModule({
